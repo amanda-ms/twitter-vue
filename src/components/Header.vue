dfa074324009fd0ed3bbf54eba0f1e9956efc0a2
Amanda Silva <amanda-ms@hotmail.com>
2020-09-16 13:42:23 -0300

    criacao header

---
+++ src/components/Header.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="sidebar">
+    <div class="sidebar__icon"></div>
+    <ul class="sidebar__list">
+      <li class="sidebar__list__item">
+        <a href="#" class="sidebar__list__item__anchor">Página Inicial</a>
+      </li>
+      <li class="sidebar__list__item">
+        <a href="#" class="sidebar__list__item__anchor">Explorar</a>
+      </li>
+      <li class="sidebar__list__item">
+        <a href="#" class="sidebar__list__item__anchor">Notificações</a>
+      </li>
+      <li class="sidebar__list__item">
+        <a href="#" class="sidebar__list__item__anchor">Mensagens</a>
+      </li>
+      <li class="sidebar__list__item">
+        <a href="#" class="sidebar__list__item__anchor">Itens salvos</a>
+      </li>
+      <li class="sidebar__list__item">
+        <a href="#" class="sidebar__list__item__anchor">Lista</a>
+      </li>
+      <li class="sidebar__list__item">
+        <a href="#" class="sidebar__list__item__anchor">Perfil</a>
+      </li>
+      <li class="sidebar__list__item">
+        <a href="#" class="sidebar__list__item__anchor">Mais</a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style lang="scss">
+.sidebar {
+  display: flex;
+  &__list {
+    display: flex;
+  }
+}
+</style>
